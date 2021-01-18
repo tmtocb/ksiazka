83b2502f363c6f3615b4fade0497c7dbb80864e5
tmtocb <kittz.esport@gmail.com>
2021-01-18 14:58:27 +0100

    add dynamic page titles, root home page, contact page with minitests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,17 +1,38 @@
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get home" do 
+  def setup
+    @base_title = "| Ruby on Rails App"
+  end
+
+
+  test "should get home page" do 
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home #{@base_title}"
   end
 
-  test "should get help" do 
+  test "should get help page" do 
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help #{@base_title}"
   end
 
-  test "should get about" do 
+  test "should get about page" do 
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About #{@base_title}"
   end
+
+  test "should get contact page" do 
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact #{@base_title}"
+  end
+
+  test "should get home as root page" do 
+    get root_url
+    assert_response :success
+    assert_select "title", "Home #{@base_title}"
+  end
+
 end
