6a97d3c4015717552a8651071acc560f1aa30456
tmtocb <kittz.esport@gmail.com>
2021-01-19 03:02:54 +0100

    define shorter routes, add paths to links

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,36 +1,25 @@
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @base_title = "| Ruby on Rails App"
-  end
-
-
-  test "should get home page" do 
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title", "Ruby on Rails App"
-  end
-
   test "should get help page" do 
-    get static_pages_help_url
+    get help_path
     assert_response :success
-    assert_select "title", "Help #{@base_title}"
+    assert_select "title", "Help | Ruby on Rails App"
   end
 
   test "should get about page" do 
-    get static_pages_about_url
+    get about_path
     assert_response :success
-    assert_select "title", "About #{@base_title}"
+    assert_select "title", "About | Ruby on Rails App"
   end
 
   test "should get contact page" do 
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
-    assert_select "title", "Contact #{@base_title}"
+    assert_select "title", "Contact | Ruby on Rails App"
   end
 
   test "should get home as root page" do 
-    get root_url
+    get root_path
     assert_response :success
     assert_select "title", "Ruby on Rails App"
   end
