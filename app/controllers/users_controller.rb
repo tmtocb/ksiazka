44cd183e6e1b7fe61cc46a4cef05cf0005ccfe70
tmtocb <kittz.esport@gmail.com>
2021-01-20 04:42:19 +0100

    make signup work, add integration tests for signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[user_params])
+    @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the App!"
+      redirect_to @user
     else
       render 'new'
     end
