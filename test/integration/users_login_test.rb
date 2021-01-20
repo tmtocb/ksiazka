1d3b99ece38d7e9bea81f17d61ddac30ab2302ab
tmtocb <kittz.esport@gmail.com>
2021-01-20 19:04:22 +0100

    add User test fixture, BCrypt digest method, add integration test for valid login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:mateusz)
+  end
+
   test "login with invalid email/password" do 
     get login_path
     assert_template 'sessions/new'
@@ -9,5 +14,26 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?    
+  end
+
+  test "login with valid email/invalid password" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: "test@test.com", password: '' }}
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?    
+  end
+
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email: @user.email, password: 'password'}}
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
