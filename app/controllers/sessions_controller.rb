690ce2a165e2d43f254dbf58616b61e02080e910
tmtocb <kittz.esport@gmail.com>
2021-01-20 15:58:20 +0100

    add successful login, include Sessions Helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log
+      reset_session
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = "invalid email/password combination"
       render 'new'
